09f237c9d11f8da8b2691bd87dd5aae5921507b4
Mike Mathew <m2mathew@me.com>
2019-02-12 09:00:37 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+// Local Variables
+const theme = createMuiTheme();
+
+// Component Definition
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <header className="App-header">
+            <a
+              className="App-link"
+              href="https://material-ui.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Material-UI
+            </a>
+          </header>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,69 @@
+export default {
+  drummers: [
+    {
+      name: 'Buddy Rich',
+      image: 'http://www.drummerworld.com/pics/drumpics34/buddyrich500999.jpg',
+      years: 'September 30, 1917 - April 2, 1987',
+    },
+    {
+      name: 'Steve Gadd',
+      image: 'http://www.drummerworld.com/drummerworld2/stevegaddbio550.jpg',
+      years: 'b. April 9, 1945',
+    },
+    {
+      name: 'Sheila E.',
+      image: 'http://www.drummerworld.com/pics/drum/dpa24/SheilaE3.jpg',
+      years: 'b. December 12, 1957',
+    },
+    {
+      name: 'Jojo Mayer',
+      image: 'http://www.drummerworld.com/drummerworld2/jojomayer550kjhg.jpg',
+      years: 'b. January 18, 1963',
+    },
+    {
+      name: 'Ahmir "Questlove" Thompson',
+      image: 'http://www.drummerworld.com/drummerworld/ahmirquestolvethompson160.jpg',
+      years: 'b. January 20, 1971',
+    },
+    {
+      name: 'Dave Grohl',
+      image: 'http://www.drummerworld.com/pics/drumpics31/davegrohlharley1.jpg',
+      years: 'b. January 14, 1969',
+    },
+    {
+      name: 'Tony Williams',
+      image: 'http://www.drummerworld.com/pics/drumpics31/davegrohlharley1.jpg',
+      years: 'December 12, 1945 - February 23, 1997',
+    },
+    {
+      name: 'Nate Smith',
+      image: 'http://www.drummerworld.com/drummerworld/natesmithcarlosibanez.jpg',
+      years: 'b. December 14, 1974',
+    },
+    {
+      name: 'Horacio "El Negro" Hernandez',
+      image: 'http://www.drummerworld.com/pics/drumpics34/horatiohernandez550987.jpg',
+      years: 'b. April 24, 1963',
+    },
+    {
+      name: 'Larnell Lewis',
+      image: 'http://www.drummerworld.com/drummerworld/larnelllewis35011.jpg',
+      years: 'b. March 22, 1984',
+    },
+    {
+      name: 'Cindy Blackman Santana',
+      image: 'http://www.drummerworld.com/drummerworld/cindyblackman350kjghh.jpg',
+      years: 'b. November 18, 1959',
+    },
+    {
+      name: 'Zakir Hussain',
+      image: 'http://www.drummerworld.com/pics/drum12/zakirhussein2.jpg',
+      years: 'b. March 9, 1951',
+    },
+    {
+      name: 'Anika Nilles',
+      image: 'http://www.drummerworld.com/drummerworld/anikanillesnorberts.jpg',
+      years: 'b. May 29, 1983',
+    },
+  ]
+};
